0638813da2bae2f1aefbb39cabf167028ab9a6a9
danielalejandroalanis <danielalejandroalanis@gmail.com>
2023-03-15 22:05:43 -0300

    Se agrega clase 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 //Componentes
 import { MainLayout } from "./layouts";
-import { PokemonCard } from "./components";
 //Estilos
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from "./App.module.css";
 
-const promesaPokemon = new Promise((resolve, reject) => {
-  const url = "https://pokeapi.co/api/v2/pokemon/?limit=20";
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      resolve(data);
+const url = "https://reqres.in/"
+
+async function getUsers() {
+  return await axios(`https://reqres.in/api/users?page=2`);
+}
+
+async function postUser(data) {
+  await axios.post(`https://reqres.in/api/users`, data, {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => console.log(res))
+}
+
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getUsers().then(res => {setData(res)})
+    postUser({
+        "name": "morpheus",
+        "job": "leader"
     })
-    .catch((error) => {
-      reject(error);
-    });
-});
-
-
-function App() {
-  const [data, setData] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
-
-  React.useEffect(() => {
-    promesaPokemon
-      .then((data) => {
-        setLoading(true);
-        setData(data.results);
-        console.log(data.results);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .then(() => {
-        setLoading(false);
-      });
-  });
-
-//   const datos = [
-//     { id: 1, name: "a", description: "pelota", stock: 2 },
-//     { id: 2, name: "b", description: "paleta", stock: 2 }
-// ]
-
+  }, [])
+ 
   return (
     <MainLayout>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <div className={styles.pokemonContainer}>
-            {data.map((pokemon, index) => {
-              return <PokemonCard pokemon={pokemon} index={index} />;
-            })}
-          </div>
-          // <div>
-          //   {
-          //     datos.map(dato => {
-          //       return (
-          //         <div key={dato.id}>
-          //           <span>Nombre: {dato.name}</span> 
-          //           <br />
-          //           <span>Descricion: {dato.description}</span>
-          //           <br />
-
-          //           <span>Stock: {dato.stock}</span>
-          //         </div>
-          //       )
-          //     })
-          //   }
-          // </div>
-        )}
+       <div style={{height: "100vh", width: "100"}}>
+        
+       </div>
     </MainLayout>
   );
 }
